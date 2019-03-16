f9ab967a42731e386eddff324da631b818610df9
akiyoshi.romeo <vw.golf.cab.range.rover.3rd@gmail.com>
2019-03-16 01:19:23 +0000

    reset for 課題：ログイン認証と一対多

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
 
-  validates :status, presence: true, length: { maximum: 10 }
+  validates :status, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 255 }
 
 end
