ebdd6888793ebeb4b553885c95ca62f881d3daf8
akiyoshi.romeo <vw.golf.cab.range.rover.3rd@gmail.com>
2019-03-18 08:18:38 +0000

    課題再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :destroy]
  
   def index
-    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    if logged_in? && current_user
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    else
+    end
   end
 
   def show
@@ -56,5 +60,12 @@ private
   def task_params
     params.require(:task).permit(:content, :status)
   end
-
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
